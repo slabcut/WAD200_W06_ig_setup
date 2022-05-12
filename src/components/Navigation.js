9b62b15a64cb095abaa55e66ef122cebe43bff4c
slabcut <dave@holycow.com.au>
2022-05-12 22:20:38 +1000

    1 binaries uploaded

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,6 @@ import "../styles/navigation.css";
 
 import React from "react";
 
-
-
 import logo from "https://raw.githubusercontent.com/slabcut/WAD200_W06_ig_setup/main/src/images/logo.png";
 
 //import { ReactComponent as HomeSVG } from "../images/home.svg";
@@ -11,12 +9,15 @@ import logo from "https://raw.githubusercontent.com/slabcut/WAD200_W06_ig_setup/
 //import { ReactComponent as ExploreSVG } from "../images/explore.svg";
 //import { ReactComponent as LikeSVG } from "../images/like.svg";
 
+//  <img className="logo" src={logo} alt="logo" />
+
+
 function Navigation() {
    return (
 
       <div className="navigation">
 
-         <img className="logo" src={logo} alt="logo" />
+<img className="logo" src={} />
 
          <div className="menu">
         poo
